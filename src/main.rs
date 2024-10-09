bfae0439f0c00f79d5d4d6e488a0928fded95fc7
Andrew <114842075+andrewjmcl@users.noreply.github.com>
2024-10-09 13:01:48 -0700

    Add "[L]oad" function
    Allows the user to load a pre-made file with a list of commands so they don't need to type tons of commands.
    
    Code is probs horribly unoptimized, commit if you can fix 👍

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
-use std::io::{self, Write};
+use std::io::{self, Write, prelude::*, BufReader};
+use std::fs::File;
+use std::path::Path;
 use std::process::Command;
+
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
+}
 
 fn main() {
     let mut asking = true;
-    println!("cmds: [Q]uit, [D]elete, [S]ubmit, [V]iew");
+    println!("cmds: [Q]uit, [D]elete, [S]ubmit, [V]iew, [L]oad");
     let mut cmds: Vec<String> = Vec::new();
 
     while asking {
         print!("$: ");
-        io::stdout().flush().unwrap(); // Ensure the prompt is displayed immediately
+        io::stdout().flush().unwrap();
 
         let mut shell_cmd = String::new();
         io::stdin()
             .read_line(&mut shell_cmd)
             .expect("Failed to read line");
-        let shell_cmd = shell_cmd.trim(); // Remove any trailing newline characters
+        let shell_cmd = shell_cmd.trim(); 
 
         match shell_cmd {
             "Q" => {
@@ -41,16 +51,30 @@ fn main() {
             "S" => {
                 asking = false;
             }
+            "L" => {
+                println!("Make sure commands are seperated by newlines.");
+                print!("File: ");
+                io::stdout().flush().unwrap(); 
+
+                let mut cmd_file = String::new();
+                io::stdin()
+                    .read_line(&mut cmd_file)
+                    .expect("Could not load file.");
+                let cmd_file = cmd_file.trim(); 
+                let lines = lines_from_file(cmd_file);
+                for line in lines {
+                    cmds.push(line);
+                }
+            }
             _ => {
                 cmds.push(shell_cmd.to_string());
             }
-        }
+        };
     }
 
     for cmd in cmds {
         println!("Executing: {}", cmd);
 
-        // Split the command into the program and its arguments
         let mut parts = cmd.split_whitespace();
         if let Some(program) = parts.next() {
             let args: Vec<&str> = parts.collect();
